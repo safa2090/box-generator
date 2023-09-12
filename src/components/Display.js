7e3a9e844f54af881fbc26ee4b097c585946dfb3
safa2090 <safadridi1987@gmail.com>
2023-09-12 20:58:42 +0100

    Box Generator

---
+++ src/components/Display.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Display = (props) => {
+  const { boxColorArray } = props;
+
+  return (
+    <div>
+      {
+        boxColorArray.map((color, index) => (
+          <div key={index} style={{ 
+              display: "inline-block",
+              margin: "10px",
+              height: "50px", 
+              width: "50px", 
+              backgroundColor: color
+              }}>
+          </div>
+        ))
+      }
+    </div>
+  );
+}
+
+export default Display;
